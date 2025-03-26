1c9b5ced29fdac75e357b8d7c1b8631f0bbff74a
Pâmela Brasil Gonella <131402720+Pamela-Brasil@users.noreply.github.com>
2025-03-26 18:19:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
+/* 
+Desenvolver uma calculadora que recebe notas de alunos e calcula a média, 
+informando se o aluno foi aprovado ou reprovado. 
+*/
+
+// Usar array para armazenar as notas. 
+
 let aluno = prompt("Informe o nome do aluno:");
 let nota = [];
 
 let promptNota = parseInt(prompt("Informe a nota do aluno:"));
 nota.push(promptNota);
 
+
+/*
+Estruturas de repetição para permitir a entrada de várias notas.
+*/
+
+
 while(confirm("Quer adicionar mais notas?") == true) {
     let promptNota2 = parseInt(prompt("Informe a nota do aluno:"));
     nota.push(promptNota2);
 }
+
+
+
+/* 
+Estruturas de decisão para determinar a aprovação (média >= 7). 
+Exibir a média e o status (aprovado/reprovado)
+*/
 
 let soma = 0;
 
@@ -23,6 +43,7 @@ document.writeln("Notas do aluno: " + mapaNota + "<br/>");
 document.writeln("Média do aluno: " + media + "<br/><br/>");
 document.writeln("");
 
+
 if(media >=7) {
         document.writeln(aluno + " aprovado! A média foi: " + media + "<br/><br/>");
     } else {
@@ -30,6 +51,14 @@ if(media >=7) {
     }
 
 document.writeln("=======================================" + "<br/><br/>");
+
+
+
+
+ /* 
+ Permitir que o usuário insira notas de vários alunos e exibir um relatório final.
+ */
+
 
 while(confirm("Quer adicionar mais alunos?") == true) {
     let aluno = prompt("Informe o nome do aluno:");
@@ -42,7 +71,8 @@ while(confirm("Quer adicionar mais alunos?") == true) {
         let promptNota2 = parseInt(prompt("Informe a nota do aluno:"));
         nota.push(promptNota2);
     }
-
+    
+     
     let soma = 0;
 
     let soma = nota.reduce((diariodenotas, nota) => diariodenotas + nota)
